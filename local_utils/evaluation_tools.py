199b257780f0bbbd7a52070111ac1e39dbaaf084
luoyao <luoyao@baidu.com>
2019-03-13 16:56:13 +0800

    add new evaluation tools

--- local_utils/evaluation_tools.py
+++ local_utils/evaluation_tools.py
@@ -5,3 +5,43 @@
 # @Site    : http://icode.baidu.com/repos/baidu/personal-code/Luoyao
 # @File    : evaluation_tools.py
 # @IDE: PyCharm
+"""
+Some evaluation tools
+"""
+import numpy as np
+
+
+def compute_accuracy(ground_truth, predictions, display=False):
+    """
+    Computes accuracy
+    :param ground_truth:
+    :param predictions:
+    :param display: Whether to print values to stdout
+    :return:
+    """
+    accuracy = []
+
+    for index, label in enumerate(ground_truth):
+        prediction = predictions[index]
+        total_count = len(label)
+        correct_count = 0
+        try:
+            for i, tmp in enumerate(label):
+                if tmp == prediction[i]:
+                    correct_count += 1
+        except IndexError:
+            continue
+        finally:
+            try:
+                accuracy.append(correct_count / total_count)
+            except ZeroDivisionError:
+                if len(prediction) == 0:
+                    accuracy.append(1)
+                else:
+                    accuracy.append(0)
+
+    accuracy = np.mean(np.array(accuracy).astype(np.float32), axis=0)
+    if display:
+        print('Mean accuracy is {:5f}'.format(accuracy))
+
+    return accuracy
