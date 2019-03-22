c1e5310baa7b289a3ba187be55aa928c20cb9dec
luoyao <luoyao@baidu.com>
2019-03-22 15:53:28 +0800

    modified the way to generate char dict and ord map dict

---
+++ data_provider/tf_io_pipline_fast_tools.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 19-3-21 下午3:03
+# @Author  : Luo Yao
+# @Site    : http://icode.baidu.com/repos/baidu/personal-code/Luoyao
+# @File    : tf_io_pipline_fast_tools.py
+# @IDE: PyCharm
--- local_utils/establish_char_dict.py
+++ local_utils/establish_char_dict.py
@@ -23,7 +23,11 @@ class CharDictBuilder(object):
 
     @staticmethod
     def _read_chars(origin_char_list):
-        """ Read a list of chars or a file containing it. """
+        """
+        Read a list of chars or a file containing it.
+        :param origin_char_list:
+        :return:
+        """
         if isinstance(origin_char_list, str):
             assert ops.exists(origin_char_list), \
                 "Character list %s is not a file or could not be found" % origin_char_list
@@ -39,6 +43,12 @@ class CharDictBuilder(object):
 
     @staticmethod
     def _write_json(save_path, data):
+        """
+
+        :param save_path:
+        :param data:
+        :return:
+        """
         if not save_path.endswith('.json'):
             raise ValueError('save path {:s} should be a json file'.format(save_path))
         os.makedirs(ops.dirname(save_path), exist_ok=True)
@@ -47,14 +57,14 @@ class CharDictBuilder(object):
 
     @staticmethod
     def write_char_dict(origin_char_list, save_path):
-        """ Writes the ordinal to char map used in int_to_char to decode predictions and labels.
+        """
+        Writes the ordinal to char map used in int_to_char to decode predictions and labels.
         The file is read with CharDictBuilder.read_char_dict()
-
         :param origin_char_list: Either a path to file with character list, one a character per line, or a list or set
                                  of characters
         :param save_path: Destination file, full path.
         """
-        char_dict = {str(ord(c)): c for c in CharDictBuilder._read_chars(origin_char_list)}
+        char_dict = {str(ord(c)) + '_ord': c for c in CharDictBuilder._read_chars(origin_char_list)}
         CharDictBuilder._write_json(save_path, char_dict)
 
     @staticmethod
@@ -71,12 +81,17 @@ class CharDictBuilder(object):
     @staticmethod
     def map_ord_to_index(origin_char_list, save_path):
         """
-            Map ord of character in origin char list into index start from 0 in order to meet the output of the DNN
+        Map ord of character in origin char list into index start from 0 in order to meet the output of the DNN
         :param origin_char_list:
         :param save_path:
         """
-        char_dict = {i: str(ord(c)) for i, c in enumerate(CharDictBuilder._read_chars(origin_char_list))}
-        CharDictBuilder._write_json(save_path, char_dict)
+        ord_2_index_dict = {str(i) + '_index': str(ord(c)) for i, c in
+                            enumerate(CharDictBuilder._read_chars(origin_char_list))}
+        index_2_ord_dict = {str(ord(c)) + '_ord': str(i) for i, c in
+                            enumerate(CharDictBuilder._read_chars(origin_char_list))}
+        total_ord_map_index_dict = dict(ord_2_index_dict)
+        total_ord_map_index_dict.update(index_2_ord_dict)
+        CharDictBuilder._write_json(save_path, total_ord_map_index_dict)
 
     @staticmethod
     def read_ord_map_dict(ord_map_dict_path):
