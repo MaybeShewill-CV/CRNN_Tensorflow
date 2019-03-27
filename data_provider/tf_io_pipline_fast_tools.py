9c77b8b0159ea7e0e0fa9c752c84d075906d15a8
luoyao <luoyao@baidu.com>
2019-03-27 16:39:28 +0800

    modified file author information

--- data_provider/tf_io_pipline_fast_tools.py
+++ data_provider/tf_io_pipline_fast_tools.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # @Time    : 19-3-21 下午3:03
-# @Author  : Luo Yao
-# @Site    : http://icode.baidu.com/repos/baidu/personal-code/Luoyao
+# @Author  : MaybeShewill-CV
+# @Site    : https://github.com/MaybeShewill-CV/CRNN_Tensorflow
 # @File    : tf_io_pipline_fast_tools.py
 # @IDE: PyCharm
 """
