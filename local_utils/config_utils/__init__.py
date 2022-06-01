acaeaaa195e14a069c77b41e83bab346a5af2b46
MaybeShewill <howard327@163.com>
2022-06-01 16:52:54 +0800

    add local utils

---
+++ local_utils/config_utils/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 2019/12/16 下午1:24
+# @Author  : MaybeShewill-CV
+# @Site    : https://github.com/MaybeShewill-CV/CRNN_Tensorflow
+# @File    : __init__.py.py
+# @IDE: PyCharm
